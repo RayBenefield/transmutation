3ef46d60ad9d8a60e3b01373afd1f20c17591a1a
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-08-23 18:40:32 -0700

    test(transmute): Starting checks for extending with null.

--- test/transmute.js
+++ test/transmute.js
@@ -3,15 +3,15 @@ import transmute from 'src'; // eslint-disable-line
 
 describe('Transmute', (it) => {
     it('does not change a null value', assert => transmute(null)
-        .then(value => assert.equal(value, null))
+        .then(value => assert.deepEqual(value, null))
     );
 
     it('does not change an integer', assert => transmute(5)
-        .then(value => assert.equal(value, 5))
+        .then(value => assert.deepEqual(value, 5))
     );
 
     it('does not change a string', assert => transmute('roar')
-        .then(value => assert.equal(value, 'roar'))
+        .then(value => assert.deepEqual(value, 'roar'))
     );
 
     it('does not change an object', assert => transmute({ test: 'roar' })
