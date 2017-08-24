302c97df2588378f31b29d6b0829188d7120e12e
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-08-23 18:23:16 -0700

    test(transmute) Added test to ensure objects do not change.

--- test/transmute.js
+++ test/transmute.js
@@ -5,10 +5,16 @@ describe('Transmute', (it) => {
     it('does not change a null value', assert => transmute(null)
         .then(value => assert.equal(value, null))
     );
+
     it('does not change an integer', assert => transmute(5)
         .then(value => assert.equal(value, 5))
     );
+
     it('does not change a string', assert => transmute('roar')
         .then(value => assert.equal(value, 'roar'))
+    );
+
+    it('does not change an object', assert => transmute({ test: 'roar' })
+        .then(value => assert.deepEqual(value, { test: 'roar' }))
     );
 });
