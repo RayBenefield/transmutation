59967a9ec6f584390c72b13208d5985651da78ec
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-08-23 18:40:32 -0700

    Starting checks for extending with null.

--- test/extend.js
+++ test/extend.js
@@ -6,12 +6,12 @@ const promise = new Promise(res => res({ test: 'roar' }));
 describe('Extend Operator', (it) => {
     it('extends a null value to a number', assert => transmute(null)
         .extend(5)
-        .then(value => assert.equal(value, 5))
+        .then(value => assert.deepEqual(value, 5))
     );
 
     it('extends a null value to a string', assert => transmute(null)
         .extend('roar')
-        .then(value => assert.equal(value, 'roar'))
+        .then(value => assert.deepEqual(value, 'roar'))
     );
 
     it('extends a null value to a object', assert => transmute(null)
@@ -27,5 +27,10 @@ describe('Extend Operator', (it) => {
     it('extends a null value to a promise result', assert => transmute(null)
         .extend(promise)
         .then(value => assert.deepEqual(value, { test: 'roar' }))
+    );
+
+    it('does not change a null value when extending with null', assert => transmute(null)
+        .extend(null)
+        .then(value => assert.deepEqual(value, null))
     );
 });
