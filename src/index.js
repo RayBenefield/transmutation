510e9e3e03f17b268ce8fce27b666d7a237a5979
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-08-23 19:12:52 -0700

    refactor(transmute): Isolate the operators completely.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-export default (value) => {
-    const transducers = [];
-    const api = {
-        extend: (obj) => {
-            transducers.push(d => (obj
-                ? Promise.resolve(obj)
-                : Promise.resolve(d)
-            ));
-            return api;
-        },
-    };
+import _ from 'lodash';
 
+const baseOperators = {
+    extend: obj => d => (obj
+        ? Promise.resolve(obj)
+        : Promise.resolve(d)
+    ),
+};
+
+const createApi = operators => transducers => (value) => {
+    const api = _.mapValues(operators, operator =>
+        (...args) => {
+            transducers.push(operator(...args));
+            return api;
+        }
+    );
     api.then = callback => transducers
         .reduce(
             (prev, next) => prev.then(next).catch(next().reject),
@@ -18,3 +22,7 @@ export default (value) => {
         .then(callback);
     return api;
 };
+
+export const configureTransmuter = operators => value => createApi(operators)([])(value);
+
+export default configureTransmuter(baseOperators);
