eb4327a6ec86ccf041d9023685d726bd7dc1faec
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-08-28 19:23:52 -0700

    test(switch): Added an additional branch to the first test.

--- test/switch.js
+++ test/switch.js
@@ -5,6 +5,7 @@ describe('Switch Operator', (it) => {
     it('selects the value that matches the scoped check', assert => transmute({ testing: { parameter: 'roar' } })
         .switch('testing.parameter', {
             roar: transmute.extend({ switchTest: 'pass' }),
+            blah: transmute.extend({ switchTest: 'fail' }),
         })
         .then(value => assert.deepEqual(value, {
             switchTest: 'pass',
@@ -13,6 +14,7 @@ describe('Switch Operator', (it) => {
             },
         }))
     );
+
     it('makes no modifications if no values match any branch', assert => transmute({ testing: { parameter: 'roar' } })
         .switch('nonexistant.parameter', {
             roar: transmute.extend({ switchTest: 'pass' }),
