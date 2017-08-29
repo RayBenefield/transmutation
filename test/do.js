0fdf9afcb4124fcde204bca19fd88d76df8d508d
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-08-28 17:55:41 -0700

    test(do): Only test one thing at a time for the do operator.

--- test/do.js
+++ test/do.js
@@ -2,8 +2,13 @@ import describe from 'tape-bdd';
 import transmute from 'src'; // eslint-disable-line
 
 describe('Do Operator', (it) => {
+    it('allows a side effect with the base value', assert => transmute({ parameter: 'roar' })
+        .do(({ parameter }) => assert.equal(parameter, 'roar'))
+        .then()
+    );
+
     it('allows a side effect without changing anything', assert => transmute({ parameter: 'roar' })
-        .do(({ parameter }) => ({ returning: `does nothing even with ${parameter}` }))
+        .do(() => ({ returning: 'does nothing' }))
         .then(value => assert.deepEqual(value, { parameter: 'roar' }))
     );
 });
