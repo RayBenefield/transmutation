b2360aaac61fceb95f401da970535c68b3b96080
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-08-28 17:57:19 -0700

    test(do): Added an additional test for doing a scoped side effect.

--- test/do.js
+++ test/do.js
@@ -16,4 +16,9 @@ describe('Do Operator', (it) => {
         .do('testing.parameter', parameter => assert.equal(parameter, 'roar'))
         .then()
     );
+
+    it('executes side effect with scoped path and still not changing anything', assert => transmute({ testing: { parameter: 'roar' } })
+        .do('testing.parameter', () => ({ returning: 'does nothing' }))
+        .then(value => assert.deepEqual(value, { testing: { parameter: 'roar' } }))
+    );
 });
