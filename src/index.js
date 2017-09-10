9663a3ea3e793ffc51d1d629428db9c2fedc9133
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-09-10 08:33:28 -0700

    feat(ifNo): Added support for arrays in the ifNo operator.

--- src/index.js
+++ src/index.js
@@ -98,6 +98,10 @@ const baseOperators = {
         return Promise.resolve(value);
     },
     ifNo: (path, transducer) => (value) => {
+        if (_.isArray(path)) {
+            if (_.every(path, p => !_.has(value, p))) return transducer(value);
+            return Promise.resolve(value);
+        }
         if (!_.has(value, path)) return transducer(value);
         return Promise.resolve(value);
     },
