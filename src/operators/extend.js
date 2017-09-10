9c4bfd33d654b2a073dfd1dc9d93c0f0eadcb3dc
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-09-10 09:23:20 -0700

    Added support for arrays in the isolate function.

--- src/operators/extend.js
+++ src/operators/extend.js
@@ -1,13 +1,5 @@
 import _ from 'lodash';
-
-const merger = (obj, value) => {
-    if (!value) return obj;
-    if (!obj) return value;
-    if (_.isArray(obj) && _.isArray(value)) return [...value, ...obj];
-    if (_.isArray(value) && !_.isArray(obj)) return [...value, obj];
-    if (!_.isPlainObject(value) || !_.isPlainObject(obj)) return value;
-    return _.mergeWith(obj, value, merger);
-};
+import merger from '../merger';
 
 export default (path, obj) => (value) => {
     // eslint-disable-next-line no-nested-ternary
