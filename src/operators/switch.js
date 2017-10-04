3eaa069cffb550b37062233b6a18448230bd5cb9
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-04 05:12:45 -0700

    feat(switch): Add default case for the switch operator.

--- src/operators/switch.js
+++ src/operators/switch.js
@@ -3,5 +3,7 @@ import _ from 'lodash';
 export default (path, branches) => (value) => {
     const test = _.get(value, path);
     if (_.has(branches, test)) return branches[test](value);
+    // eslint-disable-next-line dot-notation
+    if (_.has(branches, '_default')) return branches['_default'](value);
     return Promise.resolve(value);
 };
