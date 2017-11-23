9098f49c13a90fd5428d71e5dcbd965779066f51
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-11-22 22:04:41 -0800

    dx(build): Setup Travis CI to handle patching and testing.

---
+++ .travis/patch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    setup_git() {
+        git config --global user.email "Raymond.Benefield@gmail.com"
+        git config --global user.name "Raymond Benefield"
+    }
+
+    commit_website_files() {
+        git remote add origin-deploy https://${GH_TOKEN}@github.com/RayBenefield/transmutation.git > /dev/null 2>&1
+        git fetch origin-deploy master
+        git branch --track master origin-deploy/master
+        git reset --hard origin-deploy/master
+        yarn run patch
+    }
+
+    upload_files() {
+        git push origin-deploy HEAD:master --tags
+    }
+
+    setup_git
+    commit_website_files
+    upload_files
+else
+    echo "Not creating a patch since this is not master."
+fi
