efc7a4a39ddfa61ead3fe07d2dd236dc3a1745b8
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-11-27 17:40:32 -0800

    dx(quality): Add an enumeration of accepted scopes for a commit.
    Closes #42

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,32 @@ module.exports = {
         'footer-leading-blank': [1, 'always'],
         'header-max-length': [2, 'always', 72],
         'scope-case': [2, 'always', 'lower-case'],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                // Relates to the whole package
+                'transmutation',
+                // Parts of Transmutation
+                'transmute',
+                'extend',
+                'if',
+                'ifNo',
+                'do',
+                'switch',
+                'extendEach',
+                'doEach',
+                'under',
+                'catch',
+                'isolate',
+                // Developer Experience/Docs Scopes
+                'worklog',
+                'build',
+                'version',
+                'quality',
+                'todo',
+            ],
+        ],
         'subject-case': [2, 'always', 'sentence-case'],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'always', '.'],
