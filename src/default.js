7666fbcc899f3bf85c2d37d43edcc4bc484ff65f
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-12-09 10:02:16 -0800

    dx(build): Restructure the entire repository into a monorepo.
    Closes #16

--- src/default.js
+++
@@ -1,7 +0,0 @@
-import configureTransmuter from './configure';
-import * as operators from './operators'; // eslint-disable-line import/no-unresolved, import/extensions
-
-const defaultTransmuter = configureTransmuter(operators);
-Object.assign(defaultTransmuter, operators);
-
-export default defaultTransmuter;
